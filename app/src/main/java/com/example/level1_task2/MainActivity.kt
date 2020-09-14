1562470d33cdf124c72b1006204f62cac330201f
Zakaria Chebti <zakaria@chipta.com>
2020-09-14 11:39:23 +0200

    Made layout. Added viewBinding. Added toastMessages.

---
+++ app/src/main/java/com/example/level1_task2/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.level1_task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.level1_task2.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.submit.setOnClickListener { submit() }
+    }
+
+    private fun submit() {
+
+    }
+
+    /**
+     * Displays a successful Toast message.
+     */
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Displays an incorrect Toast message.
+     */
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_SHORT).show()
+    }
+}
